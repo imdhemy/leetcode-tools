1de7ab82cbbd038a6b1ef27b773605126f702109
Mohamad Eldhemy <imdhemy@gmail.com>
2022-07-05 21:53:48 +0300

    feat(timer): create ractive app

--- src/content/timer/timer.js
+++ src/content/timer/timer.js
@@ -10,7 +10,7 @@ const mutationObserver = new MutationObserver(mutations => {
     
     mutation.addedNodes.forEach(node => {
       if (isCodeEditor(node)) {
-        console.log(node);
+        showTimer(node);
       }
     });
   });
@@ -19,3 +19,20 @@ const mutationObserver = new MutationObserver(mutations => {
 const app = document.getElementById('app');
 
 mutationObserver.observe(app, {childList: true, subtree: true});
+
+const showTimer = (editor) => {
+  const toolBar = editor.childNodes[0].childNodes[0].childNodes[0];
+  const toolsContainer = toolBar.childNodes[2];
+  const timer = document.createElement('div');
+  timer.classList.add('tool-item__2DCU');
+  toolsContainer.insertAdjacentElement('afterbegin', timer);
+  createTimer(timer);
+};
+
+const createTimer = (el) => {
+  const ractive = Ractive({
+    target: el,
+    template: '<p>{{greeting}} {{name}}!</p>',
+    data: {greeting: 'Hello', name: 'world'},
+  });
+};
